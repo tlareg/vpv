ca53542121c856f1bd82be725a0bd1315105950b
Tlareg Aivir <tlaregaivir@gmail.com>
2016-10-24 01:46:53 +0200

    feat(lrsList) LRS list export and import from json file

--- src/app/lrs/list/lrs-list-component.js
+++ src/app/lrs/list/lrs-list-component.js
@@ -1,25 +1,58 @@
 import lrsListTemplate from './lrs-list.html';
 
 class LRSListCtrl {
-  constructor(xAPI, storageHelper) {
+  constructor($window, xAPI, storageHelper) {
     'ngInject';
 
+    this.$window = $window;
     this.xAPI = xAPI;
-    this.storageHelper = storageHelper
+    this.storageHelper = storageHelper;
+
+    this.xAPI.LRS.import(this.storageHelper.get('xAPI.LRSList'));
+    this.refreshList();
+  }
+
+  onListChange() {
+    this.storageHelper.set('xAPI.LRSList', this.xAPI.LRS.export());
     this.refreshList();
   }
 
   refreshList() {
-    const list = this.storageHelper.get('xAPI.LRSList');
-    this.xAPI.LRS.import(list);
-
     this.list = this.xAPI.LRS.getList();
   }
 
   removeLRS(lrs) {
     this.xAPI.LRS.remove(lrs);
-    this.storageHelper.set('xAPI.LRSList', this.xAPI.LRS.export());
-    this.refreshList();
+    this.onListChange();
+  }
+
+  export() {
+    let urlData = this.xAPI.LRS.export();
+    try {
+      urlData = JSON.stringify(urlData);
+    } catch(e) {
+      alert('Error on export: ' + e);
+      return;
+    }
+    const url = 'data:text/json;charset=utf8,' + encodeURIComponent(urlData);
+    this.$window.open(url, '_blank');
+    this.$window.focus();
+  }
+
+  onReadImportFile(fileContent) {
+    this.importFileContent = fileContent;
+  }
+
+  import() {
+    let contentJSON;
+    try {
+      contentJSON = JSON.parse(this.importFileContent);
+    } catch (e) {
+      alert('Error on parsing JSON from import file: ' + e);
+      return;
+    }
+    this.xAPI.LRS.import(contentJSON);
+    this.onListChange();
   }
 }
 
